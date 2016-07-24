171b33723c526e9a2e84e6b89f3b3185e56c38a2
Oleg Chernyshenko <peyot3d@gmail.com>
2016-07-24 12:31:14 +1200

    Initial Commit

---
+++ TrailsAPIKit/TrailsAPIKit.h
@@ -0,0 +1,19 @@
+//
+//  TrailsAPIKit.h
+//  TrailsAPIKit
+//
+//  Created by Oleg Chernyshenko on 24/07/16.
+//  Copyright © 2016 Oleg Chernyshenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TrailsAPIKit.
+FOUNDATION_EXPORT double TrailsAPIKitVersionNumber;
+
+//! Project version string for TrailsAPIKit.
+FOUNDATION_EXPORT const unsigned char TrailsAPIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TrailsAPIKit/PublicHeader.h>
+
+
